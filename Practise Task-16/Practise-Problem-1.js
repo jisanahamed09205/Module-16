c65538eb0d5aeb026985a0ebed2644f14ca06891
jisanahamed09205 <jisanahamed09205@gmail.com>
2023-09-09 02:57:13 +0600

    uh

---
+++ Practise Task-16/Practise-Problem-1.js
@@ -0,0 +1,7 @@
+var momMoney = 1000;
+var apple = 400;
+var orange = 300;
+var productsPrice = apple + orange;
+console.log(productsPrice);
+var returnMoney = momMoney - productsPrice;
+console.log(returnMoney);
---
+++ Practise Task-16/Practise-Problem-2.js
@@ -0,0 +1,8 @@
+var math = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+var totalMarks = math + biology + chemistry + physics + bangla;
+var avgMarks = totalMarks / 5;
+console.log(avgMarks);
